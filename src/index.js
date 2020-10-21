bd336deab951415213a7d1d842252218d953db72
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2020-10-21 14:51:26 -0400

    updated app with firebase api key

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import { App } from './app';
 import { FirebaseContext } from './context/firebase'
 
 const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-}
+    apiKey: "AIzaSyCBs_Ei6fVuO1wk3fM5xkapfvw6tPF9Rrw",
+    authDomain: "mynetflix-93ded.firebaseapp.com",
+    databaseURL: "https://mynetflix-93ded.firebaseio.com",
+    projectId: "mynetflix-93ded",
+    storageBucket: "mynetflix-93ded.appspot.com",
+    messagingSenderId: "1069482869324",
+    appId: "1:1069482869324:web:e528eb3f83a81d71e7aae9"
+  };
 
-const firebase = window.firebase.initializeApp(config);
+
 
 
 ReactDOM.render(
